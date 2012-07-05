44678f9e11744160d5682e18c922932101b78546
Maxim Piskunov <maxitg@neutrino.local>
2012-07-05 17:23:17 +0400

    Initial Commit

---
+++ CivilizationModeller/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CivilizationModeller
+//
+//  Created by Maxim Piskunov on 05.07.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
